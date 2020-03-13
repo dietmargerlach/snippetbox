e4d7a22cab8ab04da43d389f90bee67456eddcfe
dietmargerlach <didi.gerlach@kabelmail.de>
2020-03-13 11:06:30 +0100

    solved missing brace

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -21,14 +22,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-// Extract the value of the id parameter from the query string and try to
-// convert it to an integer using the strconv.Atoi() function. If it can't
-// be converted to an integer, or the value is less than 1, we return a 404 page
-// not found response.
-id, err := strconv.Atoi(r.URL.Query().Get("id"))
-if err != nil || id < 1 {
-http.NotFound(w, r)
-return
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 }
 
 // Add a createSnippet handler function.
