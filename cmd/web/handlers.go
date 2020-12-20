08804e03efd3b8c9fb70cea2831ed8f14a29c33a
dietmargerlach <didi.gerlach@kabelmail.de>
2020-12-20 15:15:44 +0100

    switched to html/template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
-	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// template set. If there's an error, we log the detailed error message and use
 	// the http.Error() function to send a generic 500 Internal Server Error
 	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl.html")
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
